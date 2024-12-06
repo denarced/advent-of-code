8a7e9fbf9928f626fc54f7f28ca5758e2f1ccfec
denarced <denarced@gmail.com>
2024-12-06 10:03:23 +0200

    Add 2024-05.

--- cli/aoc-2024-05/aoc-2024-05.go
+++ cli/aoc-2024-05/aoc-2024-05.go
@@ -11,12 +11,16 @@ import (
 func main() {
 	shared.InitLogging()
 	shared.Logger.Info("Start.")
-	file, err := os.Open("data/2024-04.txt")
+
+	file, err := os.Open("data/2024-05.txt")
 	shared.Die(err, "open file")
 	defer file.Close()
+
 	lines, err := aoc2024.ReadLines(file)
 	shared.Die(err, "ReadLines")
-	fmt.Println("XMAS count:", aoc2024.CountInTable(lines, "XMAS"))
-	fmt.Println("MAX-MAX count:", aoc2024.CountWordCrosses(lines, "MAS"))
+
+	fmt.Println("Correct middle page number sum:", aoc2024.SumCorrectMiddlePageNumbers(lines))
+	fmt.Println("Incorrect middle page number sum:", aoc2024.SumIncorrectMiddlePageNumbers(lines))
+
 	shared.Logger.Info("Done.")
 }
--- lib/aoc2024/aoc2024_test.go
+++ lib/aoc2024/aoc2024_test.go
@@ -207,3 +207,48 @@ func TestCountWordCrosses(t *testing.T) {
 		2,
 	)
 }
+
+func TestSumCorrectMiddlePageNumbers(t *testing.T) {
+	shared.InitTestLogging(t)
+	// 143 is from the problem description.
+	require.Equal(t, 143, SumCorrectMiddlePageNumbers(advent05Lines()))
+}
+
+func advent05Lines() []string {
+	// Example values from problem description.
+	return []string{
+		"47|53",
+		"97|13",
+		"97|61",
+		"97|47",
+		"75|29",
+		"61|13",
+		"75|53",
+		"29|13",
+		"97|29",
+		"53|29",
+		"61|53",
+		"97|53",
+		"61|29",
+		"47|13",
+		"75|47",
+		"97|75",
+		"47|61",
+		"75|61",
+		"47|29",
+		"75|13",
+		"53|13",
+		"75,47,61,53,29",
+		"97,61,53,29,13",
+		"75,29,13",
+		"75,97,47,61,53",
+		"61,13,29",
+		"97,13,75,29,47",
+	}
+}
+
+func TestSumIncorrectMiddlePageNumbers(t *testing.T) {
+	shared.InitTestLogging(t)
+	// 123 is from the problem description.
+	require.Equal(t, 123, SumIncorrectMiddlePageNumbers(advent05Lines()))
+}
