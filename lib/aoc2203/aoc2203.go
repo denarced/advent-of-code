c79fc7b6fcb113f0580124f564ffc6185df78656
denarced <denarced@gmail.com>
2026-08-21 19:24:06 +0300

    Add 2022-03 part 1.

---
+++ lib/aoc2203/aoc2203.go
@@ -0,0 +1,56 @@
+package aoc2203
+
+import (
+	"github.com/denarced/advent-of-code/shared"
+	"github.com/denarced/gent"
+)
+
+func SumPriorities(lines []string) int {
+	var sum int
+	for _, line := range lines {
+		contents := splitLine(line)
+		commonItem := deriveCommon(contents)
+		var added int
+		if commonItem <= 'Z' {
+			added = int(commonItem-'A') + 27
+		} else {
+			added = int(commonItem-'a') + 1
+		}
+		sum += added
+		shared.Logger.Info("Add new value.", "common", commonItem, "value", added)
+	}
+	shared.Logger.Info("Sum priorities summed.", "sum", sum)
+	return sum
+}
+
+func splitLine(line string) []string {
+	mid := len(line) / 2
+	return []string{line[:mid], line[mid:]}
+}
+
+func deriveCommon(contents []string) rune {
+	uniques := deriveUniques(contents)
+	var found rune
+	uniques[0].ForEach(func(each rune, stop func()) {
+		for i := 1; i < len(uniques); i++ {
+			if !uniques[i].Has(each) {
+				return
+			}
+		}
+		found = each
+		stop()
+	})
+	return found
+}
+
+func deriveUniques(contents []string) []*gent.Set[rune] {
+	sets := make([]*gent.Set[rune], len(contents))
+	for i, each := range contents {
+		current := gent.NewSet[rune]()
+		sets[i] = current
+		for _, c := range each {
+			current.Add(c)
+		}
+	}
+	return sets
+}
---
+++ lib/aoc2203/aoc2203_test.go
@@ -0,0 +1,19 @@
+package aoc2203
+
+import (
+	"testing"
+
+	"github.com/denarced/advent-of-code/shared"
+	"github.com/denarced/advent-of-code/shared/inr"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSumPriorities(t *testing.T) {
+	shared.InitTestLogging(t)
+	req := require.New(t)
+
+	lines, err := inr.ReadPath("testdata/in.txt")
+	req.NoError(err)
+
+	req.Equal(157, SumPriorities(lines))
+}
